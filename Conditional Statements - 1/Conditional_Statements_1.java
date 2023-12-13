2d5caac8928af5038ebdce207cb36b769707aa7b
Dan Jandel De Ramos <djderamos21@gmail.com>
2023-12-13 10:55:10 +0800

    Update Conditional_Statements_1.java

--- Conditional Statements - 1/Conditional_Statements_1.java
+++ Conditional Statements - 1/Conditional_Statements_1.java
@@ -10,17 +10,28 @@ public class Conditional_Statements_1{
     public static void main(String[]args){
 
         Scanner input = new Scanner(System.in);
-        String letter;
-        int number;
+        String letter;        
 
         //If-then-else
         System.out.println();
-        System.out.println("\u2022 If-then-Else \u2022");
+        System.out.println("\u2219 If-then-Else \u2219");
         System.out.println();
 
         //Nested-If-then-Else
         System.out.println();
         System.out.println("\u2219 Vowel or Not? \u2219");
+        System.out.print("Type a letter: ");
+        letter = input.nextLine();
+        if(letter.equalsIgnoreCase("a")||
+        letter.equalsIgnoreCase("e")||
+        letter.equalsIgnoreCase("i")||
+        letter.equalsIgnoreCase("o")||
+        letter.equalsIgnoreCase("u")
+        ){
+            System.out.println("The letter " + letter + " is a vowel.");
+        }else{
+            System.out.println("Your input is not a vowel.");
+        }
         System.out.println();
         input.close();
     }
